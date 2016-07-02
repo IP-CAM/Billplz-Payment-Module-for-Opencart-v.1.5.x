022e082d95fbe2540fb456ece4de3ed497d6c3bd
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2016-07-02 10:41:56 +0800

    Update billplz.php

--- upload/catalog/model/payment/billplz.php
+++ upload/catalog/model/payment/billplz.php
@@ -1,6 +1,9 @@
 <?php
 class ModelPaymentBillplz extends Model {
 	public function getMethod($address, $total) {
+		if ($total < $this->config->get('billplz_minlimit')){
+			return null;
+		}
 		$this->load->language('payment/billplz');
 
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('billplz_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
