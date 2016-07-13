576ecb771a625c0c529622d016c1a16fc3f0152c
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2016-07-13 19:03:49 +0800

    Update billplz.php

--- upload/catalog/controller/payment/billplz.php
+++ upload/catalog/controller/payment/billplz.php
@@ -8,25 +8,15 @@
  */
 class ControllerPaymentBillplz extends Controller
 {
+	private function get_domain_forwebmaster(){
+		return substr($_SERVER['HTTP_HOST'],0,3) == 'www' ? substr($_SERVER['HTTP_HOST'],4) : $_SERVER['HTTP_HOST'];
+	}
 	public function index()
 	{
 		$data['button_confirm'] = $this->language->get('button_confirm');
 		$this->load->model('checkout/order');
 		$order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
 		//action as a jump url
-		//number intelligence
-		$custTel    = $order_info['telephone'];
-		$custTel2   = substr($order_info['telephone'], 0, 1);
-		if ($custTel2 == '+') {
-			$custTel3 = substr($order_info['telephone'], 1, 1);
-			if ($custTel3 != '6')
-				$custTel = "+6" . $order_info['telephone'];
-		} else if ($custTel2 == '6') {
-		} else {
-			if ($custTel != '')
-				$custTel = "+6" . $order_info['telephone'];
-		}
-		//number intelligence
 		//$data['country'] = $order_info['payment_iso_code_2'];
 		//$data['currency'] = $order_info['currency_code'];
 		$products = $this->cart->getProducts();
@@ -41,8 +31,72 @@ class ControllerPaymentBillplz extends Controller
 		//Delivery Notification
 		$delivery = $this->config->get('billplz_delivery');
 		$deliver = $delivery > 0 ? true : false;
-		$orderInfoEmail = $delivery == "2" ? null : $order_info['email'];
-		$custTel = $delivery == "1" ? null : $custTel;
+
+		if ( $delivery == "2" ){
+			$orderInfoEmail = null;
+		}
+		elseif (isset($order_info['email'])) {
+			if ($order_info['email']==''){
+				$orderInfoEmail = "webmaster@" . self::get_domain_forwebmaster();
+			}
+			else {
+				$orderInfoEmail = $order_info['email'];
+			}
+		}
+		else {
+			$orderInfoEmail = "webmaster@" . self::get_domain_forwebmaster();
+		}
+		
+		if ( $delivery == "1" ){
+			$custTel = null;
+		}
+		elseif (isset($order_info['telephone'])){
+			if ($order_info['telephone']==''){
+				$custTel = '60121234567';
+			}
+			else {
+				$custTel = $order_info['telephone'];
+				//number intelligence
+				$custTel2   = substr($order_info['telephone'], 0, 1);
+				if ($custTel2 == '+') {
+					$custTel3 = substr($order_info['telephone'], 1, 1);
+					if ($custTel3 != '6')
+						$custTel = "+6" . $order_info['telephone'];
+					} else if ($custTel2 == '6') {
+					} else {
+						if ($custTel != '')
+							$custTel = "+6" . $order_info['telephone'];
+				}
+				//number intelligence
+			}
+		}
+		else {
+			$custTel = '60121234567';
+		}
+		
+		if (isset($order_info['payment_firstname'])){
+			if ($order_info['payment_firstname']==''){
+				$firstName = 'Name';
+			}
+			else {
+				$firstName = $order_info['payment_firstname'];
+			}			
+		}
+		else {
+			$firstName = 'Name';
+		}
+		
+		if (isset($order_info['payment_lastname'])){
+			if ($order_info['payment_lastname']==''){
+				$lastName = 'Name';
+			}
+			else {
+				$lastName = $order_info['payment_lastname'];
+			}			
+		}
+		else {
+			$lastName = 'Name';
+		}
 		
 		$returnurl   = $this->url->link('payment/billplz/return_ipn', '', 'SSL');
 		$callbackurl = $this->url->link('payment/billplz/callback_ipn', '', 'SSL');
@@ -52,9 +106,10 @@ class ControllerPaymentBillplz extends Controller
 			'email' => $orderInfoEmail,
 			'description' => substr("Order " . $this->session->data['order_id'] . " - ".implode($data['prod_desc']), 0, 199),
 			'mobile' => $custTel,
-			'name' => $order_info['payment_firstname'] . ' ' . $order_info['payment_lastname'],
+			'name' => $firstName . ' ' . $lastName,
 			'reference_1_label' => "ID",
 			'reference_1' => $this->session->data['order_id'],
+			'deliver' => $deliver,
 			'amount' => $amount * 100,
 			'callback_url' => $callbackurl,
 			'redirect_url' => $returnurl
@@ -81,7 +136,7 @@ class ControllerPaymentBillplz extends Controller
 		$return = curl_exec($process);
 		curl_close($process);
 		$arr            = json_decode($return, true);
-		$data['action'] = isset($arr['url']) ? $arr['url'] : "http://wanzul-hosting.com";
+		$data['action'] = isset($arr['url']) ? $arr['url'] : "http://facebook.com/billplzplugin";
 		//***************************************
 		$version_oc     = substr(VERSION, 0, 3);
 		if ($version_oc == "2.2") {
